5654a0d254dc4622dfd058353daeae4d18121cf3
RPGhugo <54813596+RPGhugo@users.noreply.github.com>
2023-06-30 00:50:09 -0300

    Add files via upload

---
+++ server/services/contents.firebase.services.js
@@ -0,0 +1,78 @@
+import { collection, getDocs, addDoc, doc, getDoc, updateDoc, deleteDoc, query, onSnapshot } from "firebase/firestore"
+import db from "../db/firebase.connection"
+
+class ContentServices {
+
+    static list (request, response) {
+        getDocs(collection(db, "content"))
+        .then(
+            (contentSnapshot)=>{
+                const contents = []
+                contentSnapshot.forEach(
+                    (content)=>{
+                        const _id = content.id
+                        const {titulo, tags, midia, descricao, link, autor} = content.data()
+                        contents.push({_id, tags, titulo, midia, descricao, link, autor})
+                    }
+                )
+                response.json(contents)
+            }
+        )
+        .catch(error=>console.log(error))
+    }
+
+
+    static register = (request, response) => {
+        addDoc(collection(db, "content"), request.body)
+            .then(
+                (docRef) => {
+                    response.json({_id:docRef.id})
+                }
+            )
+            .catch(error => console.log(error))
+    }
+
+    static retrieve(request, response) {
+        const docRef = doc(db, "content", request.params.id)
+        getDoc(docRef)
+            .then(  
+                (content) => {
+                    const res = {
+                        _id: content.id,
+                        titulo: content.data().titulo,
+                        tags: content.data().tags,
+                        midia: content.data().midia,
+                        description: content.data().descricao,
+                        link: content.data().link,
+                        author: content.data().autor,
+                    }
+                    response.json(res)
+                }
+            )
+            .catch(error => console.log(error))
+    }
+
+    static update = (request,response) => {
+        updateDoc(
+            doc(db,"content", request.params.id),
+            request.body)
+            .then(
+                () => {
+                    response.json({_id:request.params.id})
+                }
+            )
+            .catch(error => console.log(error))
+    }
+
+    static delete = (request,response) => {
+        deleteDoc(doc(db, "content", request.params.id))
+            .then(() => response.json({_id:request.params.id}))
+            .catch(error => console.log(error))
+    }
+
+
+}
+
+// module.exports = ContentServices 
+
+export default ContentServices
